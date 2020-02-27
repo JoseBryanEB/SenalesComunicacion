0dce505db90478f938d3b0c98136874450bde491
JoseBryanEB <josbryaneb@gmail.com>
2020-02-27 02:32:09 -0600

    muero de sueño alv

--- volumen.c
+++ volumen.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 #include<math.h>
 int main (int argc, char * argv[]){
-if(argc!=3)printf ("ingresar argumentos");
+if(argc!=2)printf ("El modo de uso es: volumen archivo.wav\n");
+/*
+ * Lectura de la cabecera de un archivo wave, lo que importa es cuantos canales se usan mono(uno) o estereo(dos),
+ * el numero de bits por muestra 1 (8bits) y 2(16bits)
+ * Tambien interesa reconocer que efectivamente se trate de una cabecera de archivo wave y no solo
+ * guiarse por la extencion .wav
+ * */
+FILE *file1=open(argv[1],"rb");
+
+
 return 0;
 }
